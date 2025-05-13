92c0b7ae1ec5fbf27d9ef350d6b97f3c7d42ac97
parasatiskylar <paras.sharma@iskylar.in>
2025-05-13 22:07:23 +0530

    added project setup

---
+++ hospital_management/patient_auth/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Patient(models.Model):
+    full_name = models.CharField(max_length=100)
+    age  =  models.IntegerField()
+    email =  models.TextField()
+    mobile  = models.TextField()
+    created_at =  models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.full_name
