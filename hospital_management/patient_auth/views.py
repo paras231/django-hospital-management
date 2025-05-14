d6fb7d69b5800685bf56089b635b1113712c78bf
parasatiskylar <paras.sharma@iskylar.in>
2025-05-14 22:41:01 +0530

    added views for showing all patients

--- hospital_management/patient_auth/views.py
+++ hospital_management/patient_auth/views.py
@@ -1,3 +1,10 @@
+from rest_framework import viewsets
 from django.shortcuts import render
-
+from .models import Patient
+from .serializers import PatientSerializer
 # Create your views here.
+
+
+class PatientViewSet(viewsets.ModelViewSet):
+    queryset = Patient.objects.all()
+    serializer_class = PatientSerializer
